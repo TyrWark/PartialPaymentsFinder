5c66a733dd78bf6a93409d2ae04c2836a81a20e5
TyrWark <167569189+TyrWark@users.noreply.github.com>
2024-07-19 13:08:42 -0500

    Create Tamper.js

---
+++ Tamper.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         PartialPaymentsFinderV3
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-14
+// @description  Find all payments on a sale regardless of completion status
+// @author       You
+// @match        https://us.merchantos.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/TyrWark/PartialPaymentsFinder/main/tamper.js
+// @grant        none
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+
+console.log("Fired Partial Payments")
